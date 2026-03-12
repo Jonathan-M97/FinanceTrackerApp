2490e8e5d6e23724d4530ff6bf75a44e3bb9d1ec
Jonathan-M97 <jonathan@teamapollo.com>
2026-03-12 16:55:24 -0400

    Also added a isManuallyCreated value for transaction model. this will be for future when i automatically add transactions via plaid. I want a way to know whether a transaction is created manually or not. also updated the date to be a time stamp - JM

--- app/src/main/java/com/jonathan/financetracker/data/model/Transaction.kt
+++ app/src/main/java/com/jonathan/financetracker/data/model/Transaction.kt
@@ -1,5 +1,6 @@
 package com.jonathan.financetracker.data.model
 
+import com.google.firebase.Timestamp
 import com.google.firebase.firestore.DocumentId
 import java.util.Date
 
@@ -7,11 +8,13 @@ data class Transaction(
     @DocumentId val id: String? = null,
     val description: String = "",
     val amount: Double = 0.0,
-    val date: Date = Date(),
+    val date: Timestamp = Timestamp.now(),
     val type: String = "Expense",
     val methodOfPayment: String = "Credit Card",
     val budgetName: String = "",
     val budgetId: String = "",
     val ownerId: String = "",
     val yearMonth: String = "",
+    val isManuallyCreated: Boolean = true
 )
+
